7fb062ad9eac32ff1903d7436f451b86c1bbb758
Rui Sousa <30603437+ItzaMi@users.noreply.github.com>
2022-07-17 15:56:21 +0100

    feat(Post): `isLastPost` styling

--- components/Post/index.tsx
+++ components/Post/index.tsx
@@ -2,15 +2,18 @@ import { FC } from 'react'
 
 interface Props {
   cuid: string
+  isLastPost: boolean
   title: string
 }
 
-const Post: FC<Props> = ({ cuid, title }) => {
+const Post: FC<Props> = ({ cuid, isLastPost, title }) => {
   const linkToPost = `https://www.hashnode.com/post/${cuid}`
 
   return (
     <a
-      className="block text-xl font-semibold cursor-pointer"
+      className={`block ${
+        !isLastPost ? 'mb-4' : ''
+      } max-w-fit text-xl font-semibold cursor-pointer hover:opacity-50`}
       href={linkToPost}
       rel="noreferrer"
       target="_blank"
