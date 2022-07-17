7aaf003bac3951d63088630b7c92ee16b53ce6f0
Rui Sousa <30603437+ItzaMi@users.noreply.github.com>
2022-07-17 15:12:18 +0100

    feat(components): `Input`

---
+++ components/Input/index.tsx
@@ -0,0 +1,24 @@
+import { FC } from 'react'
+
+interface Props {
+  label?: string
+  onInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  placeholder: string
+  value: string
+}
+
+const Input: FC<Props> = ({ label, onInputChange, placeholder, value }) => {
+  return (
+    <div className="flex flex-col">
+      {label && <label className="mb-2 text-xs">{label}</label>}
+      <input
+        className="px-4 h-10 w-64 border-[1px] rounded-md"
+        onChange={onInputChange}
+        placeholder={placeholder}
+        value={value}
+      />
+    </div>
+  )
+}
+
+export default Input
