c15fc0a65b36a07a5978e46198c32b3355923f52
Rui Sousa <30603437+ItzaMi@users.noreply.github.com>
2022-07-17 15:56:10 +0100

    feat(Button): add `ButtonType` style

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,15 +1,33 @@
 import { FC } from 'react'
 
+export enum ButtonType {
+  Primary = 'primary',
+  Secondary = 'secondary',
+}
+
 interface Props {
   isDisabled: boolean
   label: string
   onClick: (ev: React.MouseEvent<HTMLButtonElement>) => void
+  type: ButtonType
 }
 
-const Button: FC<Props> = ({ isDisabled, label, onClick }) => {
+const Button: FC<Props> = ({ isDisabled, label, onClick, type }) => {
+  const buttonStyle = (type: ButtonType) => {
+    switch (type) {
+      case ButtonType.Primary:
+      default:
+        return 'bg-blue-700 text-white'
+      case ButtonType.Secondary:
+        return 'bg-white text-blue-700 border-[1px] border-blue-700'
+    }
+  }
+
   return (
     <button
-      className="ml-2 px-4 h-10 bg-blue-700 rounded-md text-white disabled:opacity-50 disabled:cursor-not-allowed"
+      className={`ml-2 px-4 h-10 rounded-md ${buttonStyle(
+        type
+      )} disabled:opacity-50 disabled:cursor-not-allowed`}
       disabled={isDisabled}
       onClick={onClick}
     >
